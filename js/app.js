39c300d88cc908e27268b7a433f7f268ec94993c
juanelbarrio <juan18345@outlook.com>
2017-01-26 16:55:01 -0700

    todo

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-//alert('hola mundo');
-
-console.log('hola desde la consola de javascript');
-
 function login() {
     var usuario = document.getElementById('txtusuario').Value;
     var passwd = document.getElementById('txtpasswd').Value;
     alert('usuario:'+ usuario +',contraseña:'+ passwd);
 }
+
+$ (document).ready(function(){
+    $('#login-button').onClick(function(event){
+        var usuario = $('#txtusuario').Value;
+        var passwd = $('txtpasswd').Value;
+        event.preventDefault();
+        login(usuario,passwd);
+    });
+});
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+require 'conexion.php';
+require 'usuario.php';
+
+$usuario = $_POST['txtusuario'];
+$password = $_POST['txtpassword'];
+
+header('Content-Type: application/json');
+echo Usuario :: login($usuario,$password);
