6512df7c2284246babd2311057be34272e5f08f7
juanelbarrio <juan18345@outlook.com>
2017-01-31 16:16:59 -0700

    1

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ require 'conexion.php';
 require 'usuario.php';
 
 $usuario = $_POST['txtusuario'];
-$password = $_POST['txtpassword'];
+$passwd = $_POST['txtpassword'];
 
 header('Content-Type: application/json');
 echo Usuario :: login($usuario,$password);
